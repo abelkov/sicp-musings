20f9def39fdaaaf4a3e17148b33612345307bcce
Alexey Belkov <knots@ya.ru>
2014-07-26 20:45:34 +0400

    ex 4.1

--- 4.1-evaluator.rkt
+++ 4.1-evaluator.rkt
@@ -1,5 +1,11 @@
 #lang racket
 
+(define (reverse l)
+  (define (rev remaining so-far)
+    (if (null? remaining)
+        so-far
+        (rev (cdr remaining) (cons (car remaining) so-far))))
+  (rev l null))
 
 (define (Eval exp env)
   (cond [(Self-evaluating? exp) exp]
@@ -51,3 +57,56 @@
          (error "Unknown procedure 
                  type: Apply" 
                 procedure))))
+
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+      null
+      (cons (Eval (first-operand exps) env)
+            (list-of-values 
+             (rest-operands exps) 
+             env))))
+
+(define (eval-if exp env)
+  (if (true? (Eval (If-predicate exp) env))
+      (Eval (If-consequent exp) env)
+      (Eval (If-alternative exp) env)))
+
+(define (eval-sequence exps env)
+  (cond ((last-exp? exps) 
+         (Eval (first-exp exps) env))
+        (else 
+         (Eval (first-exp exps) env)
+         (eval-sequence (rest-exps exps) 
+                        env))))
+
+(define (eval-assignment exp env)
+  (set-variable-value! 
+   (Assignment-variable exp)
+   (eval (Assignment-value exp) env)
+   env)
+  'ok)
+
+(define (eval-definition exp env)
+  (define-variable! 
+    (Definition-variable exp)
+    (Eval (Definition-value exp) env)
+    env)
+  'ok)
+
+; ex 4.1
+
+(define (list-of-values-left exps env)
+  (if (no-operands? exps)
+      null
+      (let ([first (Eval (first-operand exps) env)])
+        (cons first
+              (list-of-values 
+               (rest-operands exps) 
+               env)))))
+
+(define (list-of-values-right exps env)
+  (if (no-operands? exps)
+      null
+      (let ([rest (list-of-values-right (rest-operands exps) env)])
+        (cons (Eval (first-operand exps) env)
+              rest))))
